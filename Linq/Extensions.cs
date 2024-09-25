9094003ad3b5983a6db5a89224649e5c1029d04f
Jonathan Krall <jonathan.krall@lexicon.se>
2024-09-25 13:29:18 +0200

    Generic Extension

---
+++ Linq/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal static class Extensions
+    {
+        //internal static IEnumerable<Person> OursWhere(this List<Person> source, Func<Person, bool> predicate)
+        //{
+        //    foreach (Person person in source)
+        //    {
+        //        if (predicate(person))
+        //        {
+        //            yield return person; 
+        //        }
+        //    }
+        //} 
+        internal static IEnumerable<T> OursWhere<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item; 
+                }
+            }
+        }
+    }
+}
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -4,7 +4,33 @@ namespace Linq
 {
     internal class Program
     {
+      
+        static void Main(string[] args)
+        {
+            IEnumerable<Person> people = GetPeople();
 
+            //var allPeopleOver30 = people.Where(p => p.Age > 30).Select(p => p.Name); 
+            var allPeopleOver30b = people.Where(IsOver30).Select(p => p.Name);
+
+            var allPeopleOver30 = people.OursWhere(p => p.Age > 30).Select(p=>p.Name);
+
+
+            foreach (var person in allPeopleOver30)
+            {
+                Console.WriteLine(person);
+            }
+            //foreach (var person in allPeopleOver30b)
+            //{
+            //    Console.WriteLine(person);
+            //}
+
+
+        }
+
+        private static bool IsOver30(Person person)
+        {
+            return person.Age > 30; 
+        }
         private static List<Person> GetPeople()
         {
             return new List<Person>
