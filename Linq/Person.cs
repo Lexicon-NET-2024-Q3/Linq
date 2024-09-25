9e605079f4e3a94763cbb98ccb9da42f63e33641
Jonathan Krall <jonathan.krall@lexicon.se>
2024-09-25 13:04:50 +0200

    Add project files.

---
+++ Linq/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Linq
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString() => $"Name: {Name} Age: {Age}";
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Linq
+{
+    internal class Program
+    {
+
+        private static List<Person> GetPeople()
+        {
+            return new List<Person>
+            {
+                new("Nisse", 20),
+                new("Nisse", 21),
+                new("Nisse", 22),
+                new("Nisse", 23),
+                new("Nisse", 24),
+                new("Nisse", 25),
+                new("Nisse", 26),
+                new("Pelle", 65),
+                new("Pelle", 60),
+                new("Pelle", 62),
+                new("Olle", 66),
+                new("Sara", 54),
+                new("Ida", 36),
+                new("Fia", 45),
+                new("Lisa", 45),
+                new("Sophia-Magdalena", 32)
+            };
+        }
+    }
+}
